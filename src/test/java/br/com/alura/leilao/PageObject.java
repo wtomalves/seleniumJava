bb1f35cfab82a2b9752cb1527c4d138e7671528f
Wellington Alves <tomalves1989@gmail.com>
2024-03-24 15:29:00 -0300

    Adicionando funcionalidade comentada

--- src/test/java/br/com/alura/leilao/PageObject.java
+++ src/test/java/br/com/alura/leilao/PageObject.java
@@ -1,5 +1,7 @@
 package br.com.alura.leilao;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,6 +15,8 @@ public class PageObject {
 		} else {
 			this.browser = browser;
 		}		
+		
+		//this.browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS).pageLoadTimeout(10, TimeUnit.SECONDS);
 	}
 	
 	public void fechar() {
